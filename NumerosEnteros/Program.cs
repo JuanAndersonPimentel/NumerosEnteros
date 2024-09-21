65538affc495d315ee91dc39db6fe7b011497cff
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-20 20:44:53 -0400

    Agregar archivos de proyecto.

---
+++ NumerosEnteros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosEnteros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int suma = 0;
+            int numero = 0;
+            
+            try
+            {
+                while (contador < 10)
+
+               Console.WriteLine($"ingrese el numero {contador + 1}:");
+                numero = Convert.ToInt32( Console.ReadLine() );
+
+                suma += numero;
+                contador++;
+
+                double promedio = (double)suma / contador;
+
+                Console.WriteLine($"la suma de los 10 numeros es: {suma}");
+                Console.WriteLine($"el promedio aritmetico es: {promedio}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error: ingreso un valor no valido. por favor, ingrese numeros enteros:");
+                
+            }
+        }
+    }
+}
---
+++ PiezasDeLote/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiezasDeLote
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const double longitudMinima = 1,2;
+            const  double longitudMaxima = 1,5;
+            int cantidadPiezas = 0;
+            int piezasAptas = 0;
+            int contador = 0;
+
+            try
+            {
+                Console.WriteLine("ingrese la cantidad de piezas en el lote: ");
+                cantidadPiezas = Convert.ToInt32(Console.ReadLine());
+
+                piezasAptas = 0;
+
+                contador = 0;
+                while (contador < cantidadPiezas)
+                {
+                    Console.WriteLine($"ingrese la longitud de la pieza {contador + 1} ( en metros): ");
+                }
+
+                double longitud = Convert.ToDouble(Console.ReadLine());
+
+                if (longitud <= longitudMinima  && longitud <= longitudMaxima)
+                {
+                    piezasAptas++;
+                }
+                contador ++;
+
+                Console.WriteLine($"la cantidad de piezas aptas para fabricar perfiles es: {piezasAptas}");
+
+            }
+            catch (Exception)
+            {
+
+               Console.WriteLine("error: ingreso un valor no valido. por favor, ingrese numeros:");
+            }
+
+
+        }
+    }
+}
